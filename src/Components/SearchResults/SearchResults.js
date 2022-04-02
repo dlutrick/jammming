e3fd5b2edc881a580268d8860222e5cb9247516e
David <davidrlutrick@gmail.com>
2022-04-02 14:22:36 -0700

    added a return statement for SearchResults.js

--- src/Components/SearchResults/SearchResults.js
+++ src/Components/SearchResults/SearchResults.js
@@ -3,9 +3,11 @@ import "./SearchResults.css";
 
 export class SearchResults extends React.Component {
   render() {
-    <div className="SearchResults">
-      <h2>Results</h2>
-      {/* <!-- Add a TrackList component --> */}
-    </div>;
+    return (
+      <div className="SearchResults">
+        <h2>Results</h2>
+        {/* <!-- Add a TrackList component --> */}
+      </div>
+    );
   }
 }
