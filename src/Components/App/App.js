34b769b36f72b7543f5fafbc631d3036e36dc11b
David <davidrlutrick@gmail.com>
2022-04-04 21:20:38 -0700

    added handleNameChange to Playlist.js, bound this to handlNameChange

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -22,6 +22,7 @@ class App extends React.Component {
     };
     this.addTrack = this.addTrack.bind(this);
     this.removeTrack = this.removeTrack.bind(this);
+    this.updatePlaylistName = this.updatePlaylistName.bind(this);
   }
 
   addTrack(track) {
@@ -41,6 +42,10 @@ class App extends React.Component {
     this.setState({ playlistTracks: result });
   }
 
+  updatePlaylistName(name) {
+    this.setState({ playlistName: name });
+  }
+
   render() {
     return (
       <div>
@@ -58,6 +63,7 @@ class App extends React.Component {
               playlistName={this.state.playlistName}
               playlistTracks={this.state.playlistTracks}
               onRemove={this.removeTrack}
+              onNameChange={this.updatePlaylistName}
             />
           </div>
         </div>
