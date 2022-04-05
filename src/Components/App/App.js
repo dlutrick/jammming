6df431cfd9043b0375168735500d72288abdd059
David <davidrlutrick@gmail.com>
2022-04-04 21:00:38 -0700

    bound the value of this to removeTrack in App.js constructor

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -21,6 +21,7 @@ class App extends React.Component {
       ],
     };
     this.addTrack = this.addTrack.bind(this);
+    this.removeTrack = this.removeTrack.bind(this);
   }
 
   addTrack(track) {
